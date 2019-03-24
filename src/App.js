616e717aba91535b99f06b3a704201fa2b74f4fc
Simon Proudfoot <simon.d.proudfoot@gmail.com>
2019-03-24 11:17:03 -0400

    first commit, setting up project following level up tuts tutorial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,153 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+// This is our first component that we created as an example of the things you can do in React
+// Feel free to un-commment it out if you want to play around with it
+// class App extends Component {
+
+//   // The constructor is called when a component is first created
+//   // Props of the component are not immediately available to the constructor unless they are passed in explicitly
+//   // constructor(props) {
+//   //   super(props);
+//   //   console.log("constructor");
+//   // }
+
+//   // This is fired BEFORE the component gets rendered/mounted
+//   // At this point, we will have access to `this`, because `this` has been created
+//   // componentWillMount() {
+//   //   console.log("will mount");
+//   // }
+
+//   // This is fired right AFTER the component gets rendered/mounted
+//   // componentDidMount() {
+//   //   console.log("did mount");
+//   // }
+
+//   state = {
+//     toggle: true,
+//     input: 'Hello'
+//   }
+
+//   toggle = () => {
+//     this.setState({
+//       toggle: !this.state.toggle
+//     })
+//   }
+
+//   updateInput = (event) => {
+//     this.setState({
+//       input: event.target.value
+//     })
+//   }
+
+//   submit = () => {
+//     console.log(this.text);
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <Welcome text="Welcome to using props"/>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//           {/* {
+//             this.state.toggle &&
+//             <p>This should show and hide</p>
+//           } */}
+//           {/* <button onClick={this.toggle}>Show/Hide</button> */}
+//           <input type="text" ref={(input) => this.text = input}/>
+//           {/* Note: the ref line is simply an arrow function that takes in the text input in as a param */}
+//           {/* From there, we assign the `text` field of the App component (which we also create by using this line) to the input itself */}
+//           {/* To confirm this, we can log this.text in the componentDidMount method */}
+//           <input type="text" onChange={this.updateInput} value={this.state.input}/>
+//           {/* We don't need to pass anything into updateInput in this case, because we will have access to the event, which we can then use to get the value of the input */}
+//           <button onClick={this.submit}>Show Value</button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// class Welcome extends Component {
+//   render() {
+//     const { text } = this.props;
+//     return (
+//       <h1 className="App-title">{text}</h1>
+//     );
+//   }
+// }
+
+// const movies = [
+//   {
+//     id: 1,
+//     title: 'Star Wars',
+//     desc: 'A movie about star stuff'
+//   },
+//   {
+//     id: 2,
+//     title: 'Spiderman'
+//   },
+//   {
+//     id: 3,
+//     title: 'Sound City'
+//   },
+//   {
+//     id: 4,
+//     title: 'Goodwill Hunting'
+//   },
+//   {
+//     id: 5,
+//     title: 'Blade Runner'
+//   },
+// ];
 
 class App extends Component {
+
+  // This is our default state for this component (in this case, the component is our entire app, so it is essentially the state for our application)
+  // In this case, this will be updated in the componentDidMount method
+  state = {
+    movies: []
+  }
+
+  // Here, we're going to use the componentDidMount lifecycle method as opposed to the constructor or componentWillMount method to fetch data from our API
+  // The reason for this is that if we put it in one of the two methods above, our UI won't render at all until that data is returned
+  // This is problematic because when we're waiting for the data to return, it could take a few seconds (during which nothing will be loaded), which isn't a good user experience
+  async componentDidMount() {
+    // The async keyword before the function allows us to run an asynchronos function, and also to await values within it (useful for situations where we have a result that we need to wait to return from somewhere, like an API)
+    // With async functions, it's a good idea to try { whatever code you want to happen } catch { catch here in case of errors }
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=12e9e2de03ebd1a7c3cfa3857fde0e3f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch(error) {
+      console.warn(error);
+    }
+  }
+
   render() {
+    console.log(this.state.movies);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {this.state.movies.map((movie) => 
+          <Movie movie={movie} desc={movie.overview} key={movie.id} />
+        )}
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// *********
+// Before converting to a functional stateless component
+// We don't really need all this code since all this component does is simply render some data
+// *********
+// export default class Movie extends Component {
+//   // Using proptypes (which is a node package we have to install) is a way of detailing what props will make an appearance in your component
+//   // You can also detail default prop types in the case that a proptype has not been passed for a given component
+//   // `static` refers to the fact that the propTypes will not change  
+//   static propTypes = {
+//     // The line below simply tells our applicaiton that we are expecting a movie prop of the type object
+//     // movie: PropTypes.object
+//     // If we were to use the line below, we would have errors in the console due to the fact that the movie prop being passed in is an object, and not a string
+//     // movie: PropTypes.string
+//     // Even better than using PropTypes.object is using PropTypes.shape, as it allows us to get more granular about what keys and value types the prop being passed in will have
+//     // The isRequired is exactly what it sounds like: it makes it so that the compiler will throw an error if that particular prop is not passed in
+//     movie: PropTypes.shape({
+//       title: PropTypes.string.isRequired,
+//       // desc: PropTypes.string
+//     }),
+//     // We have moved
+//     desc: PropTypes.string
+//   }
+  
+
+//   // defaultProps are exactly what they sound like: a way for your to have a default value for a given prop if it is not passed into the component
+//   // One of the central problems of defaultProps, however, is that we are not able to assign defaultProps to a nested prop (like description)
+//   // As a result, we need to move it outside of the movie PropType.shape and have it as it's own PropType, and also pass it in explicitly as a prop in the parent component
+//   static defaultProps = {
+//     // THIS WOULD NOT WORK
+//     // movie: PropTypes.shape({
+//     //   desc: 'Description not available'
+//     // })
+//     desc: 'Description not available'
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//       {/* Without the key attribute on the div, React will complain in the console that you need it. */}
+//       {/* You need it because React needs a way to keep track of the items in an array */}
+//       {/* In our case, it makes sense to use the ID of each movie as this key attribute, since every movie has a (hopefully) unique ID */}
+//         <h3>{this.props.movie.title}</h3>
+//         {/* The below line was necessary when desc was a sub-prop of the movie prop, but since it is being passed in as it's own prop, we use the next line down */}
+//         {/* <p>{this.props.movie.desc}</p> */}
+//         <p>{this.props.desc}</p>
+//       </div>
+//     )
+//   }
+// }
+
+// Again, because this only returns the Movie component, we're going to define Movie as a function that implicitly returns the markup for the movie
+// The prop from the parent component is passed in as `movie` (and destructured using the {} syntax) and then used to populate the Movie component
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+
+const Movie = ({ movie }) => (
+  <div>
+    <img src={`${POSTER_PATH}${movie.poster_path}`} />
+    <h3>{movie.title}</h3>
+  </div>
+);
+
+// We have this line down here (after the function definition) because in JavaScript, we cannot export a function definition
+export default Movie;
+
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
