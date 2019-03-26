cba7cba1b57040502ed94687667c94be58bdd4f8
Simon Proudfoot <simon.d.proudfoot@gmail.com>
2019-03-25 22:49:03 -0400

    Updates

---
+++ precache-manifest.9b5c6a2756a5502a83c348221db0717b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff9a163feb64cd25f5eb42dfece411a4",
+    "url": "proudfeet.github.io/moviedatabase/static/media/logo.ff9a163f.svg"
+  },
+  {
+    "revision": "e9e3bd6649e1baffdae0",
+    "url": "proudfeet.github.io/moviedatabase/static/js/runtime~main.280e071f.js"
+  },
+  {
+    "revision": "4461047568bf5efccc81",
+    "url": "proudfeet.github.io/moviedatabase/static/js/main.54a94ae7.chunk.js"
+  },
+  {
+    "revision": "602e003ae8b9aed693e4",
+    "url": "proudfeet.github.io/moviedatabase/static/js/2.9c92a766.chunk.js"
+  },
+  {
+    "revision": "4461047568bf5efccc81",
+    "url": "proudfeet.github.io/moviedatabase/static/css/main.b61ff13c.chunk.css"
+  },
+  {
+    "revision": "df55185ad0d63c61047ae725c53bca8f",
+    "url": "proudfeet.github.io/moviedatabase/index.html"
+  }
+];
